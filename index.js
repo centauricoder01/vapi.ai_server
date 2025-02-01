3f692b9498fc3eb5d3efb2a61da1df34b58ebad4
Rajendra <itsrajendrapatel@gmail.com>
2025-02-02 02:14:42 +0530

    Created New Route

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/api/v1", (req, res) => {
   res.send("<h1>Hello, This is Suzi.</h1>");
 });
 
+app.get("/api/v1/hello", (req, res) => {
+  res.send("<h1>This is After the Domain and SSL Attchment.</h1>");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
