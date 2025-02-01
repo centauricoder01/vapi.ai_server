157e0792b02034473f670c2f91fad2a6ff7a9da9
Rajendra <itsrajendrapatel@gmail.com>
2025-02-02 02:27:03 +0530

    PM2 api added

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get("/api/v1/hello", (req, res) => {
   res.send("<h1>This is After the Domain and SSL Attchment.</h1>");
 });
 
+app.get("/api/v1/pm2", (req, res) => {
+  res.send("<h1>This is After PM2 has been added.</h1>");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
